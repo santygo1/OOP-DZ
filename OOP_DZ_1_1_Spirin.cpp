c2d51db2314c8d41d9bee299f5ddf1230705f682
tygo <38673391+santygo1@users.noreply.github.com>
2021-10-25 22:44:48 +1000

    Add files via upload

---
+++ OOP_DZ_1_1_Spirin.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+double medianCalc(double a,double b, double c) {
+
+    return 0.5 * sqrt(2 * b * b + 2 * c * c - a * a);
+}
+
+double read() {
+    double a;
+    do {
+        if (!cin)
+        {
+            cout << "ERROR: INCORRECT DATA.\nINPUT CORRECT DATA:";
+            cin.clear(); 
+            cin.ignore(32767, '\n');
+        }
+        cin >> a;
+    } while (!cin);
+    return a;
+}
+
+int main()
+{
+    double a, b, c;
+
+    cout << "INPUT A:";
+    a = read();
+    cout << "INPUT B:";
+    b = read();
+    cout << "INPUT C:";
+    c = read();
+
+    cout << "Ma = " << medianCalc(a, b, c) << endl;
+    cout << "Mb = " << medianCalc(b, a, c) << endl;
+    cout << "Mc = " << medianCalc(c, a, b) << endl;
+}
---
+++ OOP_DZ_1_2_Spirin.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double getDouble() {
+    double a;
+
+    do {
+        if (!cin)
+        {
+            cout << "ERROR: INCORRECT DATA.\nINPUT CORRECT X:";
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        cin >> a;
+
+    } while (!cin);
+
+    return a;
+}
+
+float getFloat() {
+    float a;
+
+    do {
+        if (!cin)
+        {
+            cout << "ERROR: INCORRECT DATA.\nINPUT CORRECT X:";
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        cin >> a;
+
+    } while (!cin);
+
+    return a;
+}
+
+template<typename N>
+void func(N num) {
+    if (num < 0) {
+        cout << "ERROR: FOUND COMPLEX NUMBER.\n";
+        return;
+    }
+    N tmp = (1 - num * num) / (1 + num * num) * sqrt(num);
+    if (tmp == num || tmp == 0) {
+        cout << "ERROR: ZERO DIVISOR.\n";
+        return;
+    }
+    else {
+        cout << "y = " << tmp << endl;
+        cout << "z = " << (abs(num) + abs(tmp)) / (num - tmp) + abs(num / tmp) << endl;
+    }
+
+
+}
+
+
+
+int main(){
+
+    double dX;
+    cout << "Double X:\n";
+    dX = getDouble();
+    func(dX);
+
+    float fX;
+    cout << "Float X:\n";
+    fX = getFloat();
+    func(fX);
+
+}
